bb21db1caf8a829102eed369f182f59cb17c8288
外马尔 <59741206+wangjian998@users.noreply.github.com>
2025-02-11 21:42:00 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -23,31 +23,23 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
-# PACKAGES=""
-# PACKAGES="$PACKAGES curl"
-# PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
-# PACKAGES="$PACKAGES luci-app-argon-config"
-# PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-# PACKAGES="$PACKAGES luci-app-openclash"
-# PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-# PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES=""
+PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
 # 增加几个必备组件 方便用户安装iStore
-# PACKAGES="$PACKAGES fdisk"
-# PACKAGES="$PACKAGES script-utils"
-# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# PACKAGES="$PACKAGES daed"
-# PACKAGES="$PACKAGES v2raya"
-# PACKAGES="$PACKAGES mosdns"
-# PACKAGES="$PACKAGES adguardhome"
-# 输出调试信息
-echo "$(date '+%Y-%m-%d %H:%M:%S') - 不止是否调用 输出看看..."
-
-
+PACKAGES="$PACKAGES fdisk"
+PACKAGES="$PACKAGES script-utils"
+PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
